678a744f1b2f75046e588953ff2a6bc36968132e
Joaquinlanusse <ljoaquin@udesa.edu.ar>
2024-11-19 17:35:26 -0300

    agrege el h1 al index y vincule la API

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 fetch('https://dummyjson.com/recipes')
-.then(res => res.json())
-.then(console.log);
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
+})
+.catch(function(error){
+    console.log(error)
+})
