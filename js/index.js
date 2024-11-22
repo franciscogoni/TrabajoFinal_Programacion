ecf16a222c1cc42757dba7b322cc14e4ee39c6c4
Joaquinlanusse <ljoaquin@udesa.edu.ar>
2024-11-22 12:06:14 -0300

    arregle el boton de cargar mas

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-fetch('https://dummyjson.com/recipes')
+fetch('https://dummyjson.com/recipes?limit=10')
 .then(function(response){
     return response.json();
 })
@@ -6,7 +6,7 @@ fetch('https://dummyjson.com/recipes')
     console.log(data);
     let listaRecetas = document.querySelector('.recetas');
     let recetas = '';
-    for (let i=0; i<11; i++){
+    for (let i=0; i<data.recipes.length; i++){
         console.log(i);
 
        recetas = recetas + `<article>
@@ -22,3 +22,38 @@ fetch('https://dummyjson.com/recipes')
 .catch(function(error){
     console.log(error)
 }) 
+
+
+let boton = document.querySelector('.boton')
+
+let skip = 10
+
+boton.addEventListener('click', function(){
+    fetch(`https://dummyjson.com/recipes?limit=10&skip=${skip}`)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){ 
+    console.log(data);
+    let listaRecetas = document.querySelector('.recetas');
+    let recetas = '';
+    for (let i=0; i<data.recipes.length; i++){
+        console.log(i);
+
+       recetas = recetas + `<article>
+         <img src=${data.recipes[i].image} alt='' width=100 height=100>
+         <h2>${data.recipes[i].name}.</h2>
+          <p>Nivel de dificultad: ${data.recipes[i].difficulty} </p>
+          <p>Name: ${data.recipes[i].name}</p>
+          <a href='./detalle.html'>Mas detalles</a>
+        </article>`;
+        }
+    listaRecetas.innerHTML += recetas
+}) 
+.catch(function(error){
+    console.log(error)
+}) 
+
+skip = skip + 10
+
+})
