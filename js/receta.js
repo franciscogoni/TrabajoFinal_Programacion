1808588d61060991b42f37eaa1fd557062b6c72c
franciscogoni <gonif@github.com>
2024-11-26 10:45:53 -0300

    Cambios finales

--- js/receta.js
+++ js/receta.js
@@ -4,28 +4,40 @@ fetch('https://dummyjson.com/recipes')
     })
     .then(function(data) {
         console.log(data);
-        
+
         let params = location.search; 
         let recetaID = new URLSearchParams(params);
-        let buscador = recetaID.get("Buscador");
-        let id = recetaID.get("id");
+        let id = recetaID.get("id"); 
+        let categoria = recetaID.get("categoria"); 
 
-        if(id){
-            let receta = data.find(function(item){
+        if (id) {
+            let receta = data.recipes.find(function(item) {
                 return item.id == id;
-            })
-            if(receta) {
+            });
+
+            if (receta) {
+                
                 document.querySelector(".nombre_receta").textContent = receta.title;
                 document.querySelector(".instrucciones").textContent = receta.instructions;
                 document.querySelector(".receta_foto").src = receta.image;
                 document.querySelector(".receta_foto").alt = "Foto de " + receta.title;
 
-                let listaCategorias = document.querySelector(".categorias")
-            }
-              
+                
+                if (categoria) {
+                    let recetasFiltradas = data.recipes.filter(function(item) {
+                        return item.tags.includes(categoria);
+                    });
+
+                    let listaRecetas = document.querySelector(".lista_recetas");
+                    recetasFiltradas.forEach(function(receta) {
+                        let li = document.createElement("li");
+                        li.textContent = receta.title;
+                        listaRecetas.appendChild(li);
+                    });
+                }
+            } 
+        } 
+    })
+    .catch(function(error) {
+        console.error("El error es: " + error);
     });
-
-
-.catch(function(error) {
-    console.error('El error es' + error;
-});
