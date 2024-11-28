9cf02bf908460d7b2482a19ffa9c18a20873d07e
franciscogoni <gonif@github.com>
2024-11-27 21:10:57 -0300

    Hice el categories.js

--- js/categories.js
+++ js/categories.js
@@ -1,11 +1,28 @@
+let params = location.search;
+let querystring = new URLSearchParams(params);
+let categories = querystring.get("Categories"); 
+
 fetch('https://dummyjson.com/recipe/tags')
-    .then(function (response) {
+    .then(function(response) {
         return response.json(); 
     })
-    .then(function (tags){
-        console.log(tags)
-        let listacategorias = document.querySelector(".lista_categorias")
-        tags.forEach(function(categoria){
-            let li = document.querySelector("li")
-        })
+    .then(function(tags) {
+        console.log(tags); 
+
+        let listaCategorias = document.querySelector('.lista_categorias');
+
+       
+        for (let index = 0; index < tags.length; index++) {
+            let category = tags[index]; 
+            listaCategorias.innerHTML += `
+                <li>
+                    <a href="./category.html?id=${category}">
+                        ${category}
+                    </a>
+                </li>`;
+        }
     })
+    .catch(function(error) {
+        console.error("El error es: " + error); 
+    });
+
