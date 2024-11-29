02b700fa5d0226d9444923ff0820a12f32513b5e
franciscogoni <gonif@github.com>
2024-11-29 11:31:37 -0300

    Cambios en categories

--- js/categories.js
+++ js/categories.js
@@ -15,7 +15,7 @@ fetch('https://dummyjson.com/recipe/tags')
         for (let index = 0; index < tags.length; index++) {
             let category = tags[index]; 
             listaCategorias.innerHTML += `
-                <article>
+                <article class ="box">
                     <a class = "link_receta" href="./category.html?id=${category}">
                         ${category}
                     </a>
